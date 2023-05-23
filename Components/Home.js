7997e545b68e3e57eeb977e022cd047285f068ff
3ab <129817739+3abdelrahmanMohamed@users.noreply.github.com>
2023-05-23 13:49:05 +0300

    thepush
    initial

---
+++ Components/Home.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function Home() {
+
+    const [Trips, setTrips] = useState([
+        {num: 1, tripname: "London", rate: 2, food: "british", },
+        {num: 2, tripname: "Mexico", rate: 3, food: "american", weather: 21},
+        {num: 3, tripname: "Singapore", rate: 5, food: "asian", weather: 21}
+    ])
+
+    const [StartDate, setStartDate]  = useState('')
+    const [EndDate, SetEndDate] = useState('')
+    const [Budget, SetBudget] = useState('')
+
+
+
+    const handleClick = (name) => {
+        console.log("You added a trip" + name)
+
+    }
+
+    const handleChange = (Event) => {
+        console.log(Event.target.value)
+    }
+
+
+
+
+  return (
+    <div>
+        <input type='date' placeholder='Enter Start Date'  onChange={handleChange}/>
+        <button onClick={() => handleClick(' Start Date')}> Add </button>
+        <input type='date' placeholder='Enter End Date' />
+        <button onClick={() => handleClick(' End Date')}> Add </button>
+        <input placeholder="Input Budget amount" />
+        <button onClick={() => handleClick(' Budget Amount')}> Add </button>
+        {
+            Trips.map((trip) =>  (
+                <div className='myTrips' key={trip.num}>
+                    <h3> {trip.tripname} </h3>
+                    <p> Rate: {trip.rate} </p>
+                    <p> Food: {trip.food} </p>
+                    <p> Weather: {trip.weather} celsuius </p>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Home
